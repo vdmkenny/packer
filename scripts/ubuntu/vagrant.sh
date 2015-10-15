fefe7e7222d3ccb4b567416b3f5777fdbed8991b
bjanssens <bjanssens@inuits.eu>
2015-10-15 21:18:35 +0200

    Ubuntu box building
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

---
+++ scripts/ubuntu/vagrant.sh
@@ -0,0 +1,7 @@
+#!/usr/vin/env bash
+
+mkdir /home/vagrant/.ssh
+wget --no-check-certificate -O authorized_keys 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub'
+mv authorized_keys /home/vagrant/.ssh/
+chown -R vagrant:vagrant /home/vagrant/.ssh
+chmod -R go-rwsx /home/vagrant/.ssh
