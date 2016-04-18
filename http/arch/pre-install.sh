96a8efca38ccdb80f89a1868d7ac8daeab73d83d
bjanssens <bjanssens@inuits.eu>
2016-04-18 23:18:56 +0200

    Arch: Add some more verbose stuff to the pre-install
    Signed-off-by: bjanssens <bjanssens@inuits.eu>

--- http/arch/pre-install.sh
+++ http/arch/pre-install.sh
@@ -2,10 +2,14 @@
 
 vagrant_passwd='vagrant'
 
+echo "Status: Adding the vagrant user"
 /usr/bin/useradd -d /home/vagrant -G wheel -m vagrant
+echo "Status: Setting the vagrant user passwd to $vagrant_passwd"
 echo "vagrant:$vagrant_passwd" | chpasswd
+echo "Status: Configuring sudo for vagrant user"
 cat > /etc/sudoers.d/10_vagrant << EOF
 Defaults:vagrant !requiretty
   vagrant ALL=(ALL) NOPASSWD: ALL
 EOF
+echo "Status: Enableing sshd"
 systemctl start sshd
