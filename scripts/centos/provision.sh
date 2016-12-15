312595fcf04a50d6abbf49d2631ef3f61a0a602c
Kenny Van de Maele <vdmkenny@inuits.eu>
2016-12-15 16:36:40 +0100

    fixed cleanup of docker provisioning
    Signed-off-by: Kenny Van de Maele <vdmkenny@inuits.eu>

--- scripts/centos/provision.sh
+++ scripts/centos/provision.sh
@@ -6,14 +6,12 @@ echo "Updating System"
 yum update -y
 
 echo "Finished update, installing EPEL"
-cd /tmp/
-curl -O http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
-rpm -ivh epel-release-7-8.noarch.rpm
+yum install epel-release -y
 
 echo "Installing custom packages"
 yum --enablerepo=epel install $PACKAGES -y
 
 echo "Removing EPEL and cleaning up"
 yum remove epel-release -y
-yum remove gcc kernel-devel kernel-headers dkms make bzip2 perl -y
-package-cleanup --quiet --leaves --exclude-bin | xargs yum remove -y
+yum remove gcc kernel-devel kernel-headers dkms make bzip2 -y
+yum autoremove -y
